c4aede0e03910242e0f263508132308a49a0f612
Theo Brigitte <theo.brigitte@gmail.com>
2019-09-30 11:40:56 +0200

    Update to kubernetes-1.15.4

--- vendor/github.com/giantswarm/apiextensions/pkg/apis/application/v1alpha1/chart_types.go
+++ vendor/github.com/giantswarm/apiextensions/pkg/apis/application/v1alpha1/chart_types.go
@@ -1,6 +1,7 @@
 package v1alpha1
 
 import (
+	"github.com/ghodss/yaml"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -8,6 +9,69 @@ import (
 const (
 	kindChart = "Chart"
 )
+
+const chartCRDYAML = `
+apiVersion: apiextensions.k8s.io/v1beta1
+kind: CustomResourceDefinition
+metadata:
+  name: charts.application.giantswarm.io
+spec:
+  group: application.giantswarm.io
+  scope: Namespaced
+  version: v1alpha1
+  names:
+    kind: Chart
+    plural: charts
+    singular: chart
+  subresources:
+    status: {}
+  validation:
+    openAPIV3Schema:
+      properties:
+        spec:
+          type: object
+          properties:
+            name:
+              type: string
+            namespace:
+              type: string
+            config:
+              type: object
+              properties:
+                configMap:
+                  type: object
+                  properties:
+                    name:
+                      type: string
+                    namespace:
+                      type: string
+                    resourceVersion:
+                      type: string
+                  required: ["name", "namespace"]
+                secret:
+                  type: object
+                  properties:
+                    name:
+                      type: string
+                    namespace:
+                      type: string
+                    resourceVersion:
+                      type: string
+                  required: ["name", "namespace"]
+            tarballURL:
+              type: string
+              format: uri
+          required: ["name", "namespace", "tarballURL"]
+`
+
+var chartCRD *apiextensionsv1beta1.CustomResourceDefinition
+
+func init() {
+	err := yaml.Unmarshal([]byte(chartCRDYAML), &chartCRD)
+	if err != nil {
+		panic(err)
+	}
+}
 
 // NewChartCRD returns a new custom resource definition for Chart.
 // This might look something like the following.
@@ -26,33 +90,12 @@ const (
 //         singular: chart
 //
 func NewChartCRD() *apiextensionsv1beta1.CustomResourceDefinition {
-	return &apiextensionsv1beta1.CustomResourceDefinition{
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: apiextensionsv1beta1.SchemeGroupVersion.String(),
-			Kind:       "CustomResourceDefinition",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "charts.application.giantswarm.io",
-		},
-		Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
-			Group:   "application.giantswarm.io",
-			Scope:   "Namespaced",
-			Version: "v1alpha1",
-			Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
-				Kind:     "Chart",
-				Plural:   "charts",
-				Singular: "chart",
-			},
-			Subresources: &apiextensionsv1beta1.CustomResourceSubresources{
-				Status: &apiextensionsv1beta1.CustomResourceSubresourceStatus{},
-			},
-		},
-	}
+	return chartCRD.DeepCopy()
 }
 
 func NewChartTypeMeta() metav1.TypeMeta {
 	return metav1.TypeMeta{
-		APIVersion: version,
+		APIVersion: SchemeGroupVersion.String(),
 		Kind:       kindChart,
 	}
 }
@@ -60,6 +103,36 @@ func NewChartTypeMeta() metav1.TypeMeta {
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
+// Chart CRs might look something like the following.
+//
+//    apiVersion: application.giantswarm.io/v1alpha1
+//    kind: Chart
+//    metadata:
+//      name: "prometheus"
+//      labels:
+//        chart-operator.giantswarm.io/version: "1.0.0"
+//
+//    spec:
+//      name: "prometheus"
+//      namespace: "monitoring"
+//      config:
+//        configMap:
+//        name: "prometheus-values"
+//        namespace: "monitoring"
+//        resourceVersion: ""
+//      secret:
+//        name: "prometheus-secrets"
+//        namespace: "monitoring"
+//        resourceVersion: ""
+//      tarballURL: "https://giantswarm.github.com/app-catalog/prometheus-1-0-0.tgz"
+//
+//    status:
+//      appVersion: "2.4.3" # Optional value from Chart.yaml with the version of the deployed app.
+//      release:
+//        lastDeployed: "2018-11-30T21:06:20Z"
+//        status: "DEPLOYED"
+//      version: "1.1.0" # Required value from Chart.yaml with the version of the chart.
+//
 type Chart struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata"`
@@ -73,9 +146,6 @@ type ChartSpec struct {
 	// Name is the name of the Helm chart to be deployed.
 	// e.g. kubernetes-prometheus
 	Name string `json:"name" yaml:"name"`
-	// KubeConfig is the kubeconfig to connect to the cluster when deploying
-	// the app.
-	KubeConfig ChartSpecKubeConfig `json:"kubeConfig" yaml:"kubeConfig"`
 	// Namespace is the namespace where the chart should be deployed.
 	// e.g. monitoring
 	Namespace string `json:"namespace" yaml:"namespace"`
@@ -117,21 +187,27 @@ type ChartSpecConfigSecret struct {
 	ResourceVersion string `json:"resourceVersion" yaml:"resourceVersion"`
 }
 
-type ChartSpecKubeConfig struct {
-	// Secret references a secret containing the kubconfig.
-	Secret ChartSpecKubeConfigSecret `json:"secret" yaml:"secret"`
-}
-
-type ChartSpecKubeConfigSecret struct {
-	// Name is the name of the secret containing the kubeconfig,
-	// e.g. chart-operator-kubeconfig.
-	Name string `json:"name" yaml:"name"`
-	// Namespace is the namespace of the secret containing the kubeconfig,
-	// e.g. giantswarm.
-	Namespace string `json:"namespace" yaml:"namespace"`
-}
-
 type ChartStatus struct {
+	// AppVersion is the value of the AppVersion field in the Chart.yaml of the
+	// deployed chart. This is an optional field with the version of the
+	// component being deployed.
+	// e.g. 0.21.0.
+	// https://docs.helm.sh/developing_charts/#the-chart-yaml-file
+	AppVersion string `json:"appVersion" yaml:"appVersion"`
+	// Reason is the description of the last status of helm release when the chart is
+	// not installed successfully, e.g. deploy resource already exists.
+	Reason string `json:"reason,omitempty" yaml:"reason,omitempty"`
+	// Release is the status of the Helm release for the deployed chart.
+	Release ChartStatusRelease `json:"release" yaml:"release"`
+	// Version is the value of the Version field in the Chart.yaml of the
+	// deployed chart.
+	// e.g. 1.0.0.
+	Version string `json:"version" yaml:"version"`
+}
+
+type ChartStatusRelease struct {
+	// LastDeployed is the time when the deployed chart was last deployed.
+	LastDeployed DeepCopyTime `json:"lastDeployed" yaml:"lastDeployed"`
 	// Status is the status of the deployed chart,
 	// e.g. DEPLOYED.
 	Status string `json:"status" yaml:"status"`
